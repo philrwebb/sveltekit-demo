937e63dfaa945d6479e5038accbd69b10f4c9f74
philrwebb <prwebb@optusnet.com.au>
2021-04-27 17:02:11 +0800

    fixed asynchronous problem with covidstats

--- src/routes/covidstats/index.svelte
+++ src/routes/covidstats/index.svelte
@@ -1,56 +1,101 @@
 <script>
-        import { covidSummary } from "$lib/stores.js";
+        import http from "$lib/httpStore.js";
         import numeral from "numeral";
-        const { Global, Countries } = $covidSummary;
-        // console.log(Countries.filter(c => c.Country.startsWith("Aus")));
+        const covidSummary = http({});
+        covidSummary.subscribe(($covidSummary) =>
+                console.log("summary loaded")
+        );
+        covidSummary.get("https://api.covid19api.com/summary");
 </script>
 
-<!-- <p>{filtertext}</p>
-<input :value={filtertext} type="text" /> -->
-<ul class="countriescontainer">
-        <ul class="headingcontainer">
-                <li>Country</li>
-                <li>Country Code</li>
-                <li>New Confirmed</li>
-                <li>Total Confirmed</li>
-                <li>New Deaths</li>
-                <li>Total Deaths</li>
-                <li>New Recovered</li>
-                <li>Total Recovered</li>
+{#if $covidSummary.Countries && $covidSummary.Global}
+        <ul class="countriescontainer">
+                <ul class="headingcontainer">
+                        <li>Country</li>
+                        <li>Country Code</li>
+                        <li>New Confirmed</li>
+                        <li>Total Confirmed</li>
+                        <li>New Deaths</li>
+                        <li>Total Deaths</li>
+                        <li>New Recovered</li>
+                        <li>Total Recovered</li>
+                </ul>
+                <ul class="worldcontainer">
+                        <li>World</li>
+                        <li>-</li>
+                        <li class="statistic">
+                                {numeral(
+                                        $covidSummary.Global.NewConfirmed
+                                ).format("0,0")}
+                        </li>
+                        <li class="statistic">
+                                {numeral(
+                                        $covidSummary.Global.TotalConfirmed
+                                ).format("0,0")}
+                        </li>
+                        <li class="statistic">
+                                {numeral($covidSummary.Global.NewDeaths).format(
+                                        "0,0"
+                                )}
+                        </li>
+                        <li class="statistic">
+                                {numeral(
+                                        $covidSummary.Global.TotalDeaths
+                                ).format("0,0")}
+                        </li>
+                        <li class="statistic">
+                                {numeral(
+                                        $covidSummary.Global.NewRecovered
+                                ).format("0,0")}
+                        </li>
+                        <li class="statistic">
+                                {numeral(
+                                        $covidSummary.Global.TotalRecovered
+                                ).format("0,0")}
+                        </li>
+                </ul>
+                {#each $covidSummary.Countries as country}
+                        <li>
+                                <ul class="countrycontainer">
+                                        <li>{country.Country}</li>
+                                        <li>{country.CountryCode}</li>
+                                        <li class="statistic">
+                                                {numeral(
+                                                        country.NewConfirmed
+                                                ).format("0,0")}
+                                        </li>
+                                        <li class="statistic">
+                                                {numeral(
+                                                        country.TotalConfirmed
+                                                ).format("0,0")}
+                                        </li>
+                                        <li class="statistic">
+                                                {numeral(
+                                                        country.NewDeaths
+                                                ).format("0,0")}
+                                        </li>
+                                        <li class="statistic">
+                                                {numeral(
+                                                        country.TotalDeaths
+                                                ).format("0,0")}
+                                        </li>
+                                        <li class="statistic">
+                                                {numeral(
+                                                        country.NewRecovered
+                                                ).format("0,0")}
+                                        </li>
+                                        <li class="statistic">
+                                                {numeral(
+                                                        country.TotalRecovered
+                                                ).format("0,0")}
+                                        </li>
+                                </ul>
+                        </li>
+                {/each}
         </ul>
-        <ul class="worldcontainer">
-                <li>World</li>
-                <li>-</li>
-                <li class="statistic">{numeral(Global.NewConfirmed).format('0,0')}</li>
-                <li class="statistic">{numeral(Global.TotalConfirmed).format('0,0')}</li>
-                <li class="statistic">{numeral(Global.NewDeaths).format('0,0')}</li>
-                <li class="statistic">{numeral(Global.TotalDeaths).format('0,0')}</li>
-                <li class="statistic">{numeral(Global.NewRecovered).format('0,0')}</li>
-                <li class="statistic">{numeral(Global.TotalRecovered).format('0,0')}</li>
-        </ul>
-        {#each Countries as country}
-                <li>
-                        <ul class="countrycontainer">
-                                <li>{country.Country}</li>
-                                <li>{country.CountryCode}</li>
-                                <li class="statistic">
-                                        {numeral(country.NewConfirmed).format('0,0')}
-                                </li>
-                                <li class="statistic">
-                                        {numeral(country.TotalConfirmed).format('0,0')}
-                                </li>
-                                <li class="statistic">{numeral(country.NewDeaths).format('0,0')}</li>
-                                <li class="statistic">{numeral(country.TotalDeaths).format('0,0')}</li>
-                                <li class="statistic">
-                                        {numeral(country.NewRecovered).format('0,0')}
-                                </li>
-                                <li class="statistic">
-                                        {numeral(country.TotalRecovered).format('0,0')}
-                                </li>
-                        </ul>
-                </li>
-        {/each}
-</ul>
+{:else}
+        <p>loading</p>
+{/if}
 
 <style>
         ul {
