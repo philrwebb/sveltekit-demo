ed1d36b823d412adb79db3df4d71a426b569bf4e
philrwebb <prwebb@optusnet.com.au>
2022-03-09 13:43:07 +0800

    updating

--- src/lib/tabs/tab.svelte
+++ src/lib/tabs/tab.svelte
@@ -1,20 +1,14 @@
 <script>
-	import {onDestroy} from 'svelte';
-	import {tabsdata} from './tabs.js';
+	import { onDestroy } from "svelte";
+	import { tabsdata } from "./tabs.js";
 	export let title;
 	export let id;
 	registerTab(id, title);
 	onDestroy(() => {
-		unregisterTab(id)
-	})
-	function updateTitle(id, title) {
-		const tabIndex = $tabsdata.titles.findIndex((title) => title.id === id);
-		if (tabIndex > -1) {
-			$tabsdata.titles[tabIndex].title = title;
-		}
-	}
+		unregisterTab(id);
+	});
 	function registerTab(id, title) {
-		$tabsdata.titles = [{id, title}, ...$tabsdata.titles];
+		$tabsdata.titles = [{ id, title }, ...$tabsdata.titles];
 	}
 	function unregisterTab(id) {
 		const tabIndex = $tabsdata.titles.findIndex((title) => title.id === id);
@@ -24,9 +18,9 @@
 		}
 	}
 </script>
+
 <div>
 	{#if $tabsdata.selectedTab === id}
-		<slot />	
+		<slot />
 	{/if}
 </div>
-
